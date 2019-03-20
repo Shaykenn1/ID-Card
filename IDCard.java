caa2b1452f1f8ba53651dd091b7f4e143116337b
Shayla Kennedy <Shaylakennedy@yahoo.com>
2019-03-19 21:05:59 -0500

    Add files via upload

---
+++ IDCard.java
@@ -0,0 +1,25 @@
+package variables_example;
+
+public class IDCard {
+	
+	public static void main(String[] args) {
+		
+		//Declaring variables for ID card
+		
+		String name;
+		String ID;
+		String state;
+		String ExpDate;
+		
+		name = "Shayla Kennedy";
+		ID = "ST05GH23";
+		state = "Mo";
+		ExpDate = "12/2023";
+		
+		System.out.println("Name: " + name);
+		System.out.println("ID: " + ID);
+		System.out.println("STATE: " + state);
+		System.out.println("EXP. DATE: " + ExpDate );
+	}
+
+}
